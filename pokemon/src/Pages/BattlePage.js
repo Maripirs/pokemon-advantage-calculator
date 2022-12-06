f8e703e87b4e82cd506fee83287b36eaad62c781
davidhlee94 <davidhlee94@gmail.com>
2022-12-06 09:55:22 -0800

    Added basic HTML for BattlePage

--- pokemon/src/Pages/BattlePage.js
+++ pokemon/src/Pages/BattlePage.js
@@ -3,7 +3,16 @@ import Results from "../Components/Results";
 import "./BattlePage.css"
 
 const BattlePage = () => {
-
+ return(
+    <>
+        <div className="battle-container">
+            <Card/>
+            <h3>VS.</h3>
+            <Card/>
+        </div>
+        <Results/>
+    </>
+ )
 
 }
 
