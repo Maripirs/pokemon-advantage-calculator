c443b747ccb3e6b98267a1bdd6337432d89c253b
Maripirs <mariapazmaluenda@gmail.com>
2022-12-08 15:13:10 -0800

    added typeColors

---
+++ pokemon/src/Components/TypeColors.js
@@ -0,0 +1,24 @@
+const TypeColors = {
+    normal: '#A9AA78',
+    fighting: '#C12C24',
+    flying: '#AA91F1',
+    poison: '#A13EA1',
+    ground: '#E1C168',
+    rock: '#B9A235',
+    bug: '#A9B91B',
+    ghost: '#71579A',
+    steel: '#FEFEFF',
+    fire: '#F0812C',
+    water: '#6891F1',
+    grass: '#78CA4F',
+    electric: '#F8D12D',
+    psychic: '#F85789',
+    ice:  '#99DAD9',
+    dragon: '#7035F8',
+    dark: '#988579',
+    fairy: 'pink',
+    unknown: 'white',
+    shadow: '#FF9CE2'
+}
+
+export default TypeColors
