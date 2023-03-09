6bc570fb119ddcdef079b133d08cb90d1ecb669f
Maripirs <mariapazmaluenda@gmail.com>
2023-03-09 10:34:59 -0800

    battle page is responsive now

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,94 +5,112 @@ import { useState, useEffect } from "react";
 import TypeImg from "./TypeImg";
 
 const Card = (pokemon, pokemonState, index) => {
-  const [colors, setColors] = useState(["red", "blue"]);
+    const [colors, setColors] = useState(["red", "blue"]);
 
-  useEffect(() => {
-    if (pokemon) {
-      let color1 = TypeColors[pokemon.types[0].type.name];
-      let color2 = color1;
-      if (pokemon.types[1]) {
-        color2 = TypeColors[pokemon.types[1].type.name];
-      }
-      setColors([color1, color2]);
-    }
-  }, [pokemon]);
+    useEffect(() => {
+        if (pokemon) {
+            let color1 = TypeColors[pokemon.types[0].type.name];
+            let color2 = color1;
+            if (pokemon.types[1]) {
+                color2 = TypeColors[pokemon.types[1].type.name];
+            }
+            setColors([color1, color2]);
+        }
+    }, [pokemon]);
 
-  return (
-    <div className="card-score">
-      <div
-        className="cards-container"
-        style={{
-          background: `linear-gradient(#fff 0 0) padding-box, linear-gradient(to right, ${colors[0]} 50%, ${colors[1]} 50%) border-box`,
-          color: `#313149`,
-          padding: `1vh`,
-          border: `1vh solid transparent`,
-          borderRadius: `2vh`,
-        }}
-      >
-        <div className="card-top">
-          <h3 className="card-name">
-            {pokemon
-              ? pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
-              : "loading..."}
-          </h3>
-          <div className="type-container">
-            <div className="type" style={{ backgroundColor: `${colors[0]}` }}>
-              <p className="type-text">
-                {pokemon
-                  ? pokemon.types[0].type.name.charAt(0).toUpperCase() +
-                    pokemon.types[0].type.name.slice(1)
-                  : "loading..."}{" "}
-              </p>
-            </div>
-            {pokemon ? (
-              pokemon.types[1] ? (
-                <div
-                  className="type"
-                  style={{ backgroundColor: `${colors[1]}` }}
-                >
-                  <p className="type-text">
-                    {pokemon.types[1].type.name.charAt(0).toUpperCase() +
-                      pokemon.types[1].type.name.slice(1)}
-                  </p>
+    return (
+        <div className="card-score">
+            <div
+                className="cards-container"
+                style={{
+                    background: `linear-gradient(#fff 0 0) padding-box, linear-gradient(to right, ${colors[0]} 50%, ${colors[1]} 50%) border-box`,
+                    color: `#313149`,
+                    // padding: `1vh`,
+                    // border: `1vh solid transparent`,
+                    // borderRadius: `2vh`,
+                }}
+            >
+                <div className="card-top">
+                    <h3 className="card-name">
+                        {pokemon
+                            ? pokemon.name.charAt(0).toUpperCase() +
+                              pokemon.name.slice(1)
+                            : "loading..."}
+                    </h3>
+                    <div className="type-container">
+                        <div
+                            className="type"
+                            style={{ backgroundColor: `${colors[0]}` }}
+                        >
+                            <p className="type-text">
+                                {pokemon
+                                    ? pokemon.types[0].type.name
+                                          .charAt(0)
+                                          .toUpperCase() +
+                                      pokemon.types[0].type.name.slice(1)
+                                    : "loading..."}{" "}
+                            </p>
+                        </div>
+                        {pokemon ? (
+                            pokemon.types[1] ? (
+                                <div
+                                    className="type"
+                                    style={{
+                                        backgroundColor: `${colors[1]}`,
+                                    }}
+                                >
+                                    <p className="type-text">
+                                        {pokemon.types[1].type.name
+                                            .charAt(0)
+                                            .toUpperCase() +
+                                            pokemon.types[1].type.name.slice(1)}
+                                    </p>
+                                </div>
+                            ) : (
+                                <p></p>
+                            )
+                        ) : (
+                            <p></p>
+                        )}
+                    </div>
                 </div>
-              ) : (
-                <p></p>
-              )
-            ) : (
-              <p></p>
-            )}
-          </div>
-        </div>
+                <div className="card-bottom">
+                    <div
+                        className="img-container"
+                        style={
+                            pokemon
+                                ? {
+                                      backgroundImage: `url(${
+                                          TypeImg[pokemon.types[0].type.name]
+                                      })`,
+                                  }
+                                : { backgroundImage: `url(${TypeImg.normal})` }
+                        }
+                    >
+                        {pokemon ? (
+                            <img
+                                className="pokemon-img"
+                                src={pokemon.sprites.front_default}
+                            />
+                        ) : (
+                            "loading..."
+                        )}
+                    </div>
+                    <Moves
+                        pokemon={pokemon}
+                        pokemonState={pokemonState}
+                        index={index}
+                    />
+                </div>
+            </div>
 
-        <div
-          className="img-container"
-          style={
-            pokemon
-              ? {
-                  backgroundImage: `url(${
-                    TypeImg[pokemon.types[0].type.name]
-                  })`,
-                }
-              : { backgroundImage: `url(${TypeImg.normal})` }
-          }
-        >
-          {pokemon ? (
-            <img className="pokemon-img" src={pokemon.sprites.front_default} />
-          ) : (
-            "loading..."
-          )}
+            <div className="pokemon-score">
+                <h3 className="score-text">
+                    Score : {pokemonState.pokemonInBattle[index].pokemonScore}
+                </h3>
+            </div>
         </div>
-        <Moves pokemon={pokemon} pokemonState={pokemonState} index={index} />
-      </div>
-
-      <div className="pokemon-score">
-        <h3 className="score-text">
-          Score : {pokemonState.pokemonInBattle[index].pokemonScore}
-        </h3>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Card;
--- src/Pages/BattlePage.js
+++ src/Pages/BattlePage.js
@@ -1,131 +1,126 @@
 import Card from "../Components/Card";
 import Results from "../Components/Results";
 import "./BattlePage.css";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 const BattlePage = () => {
-
     //Setting use state for pokemon 1 and 2
-  const [pokemon1, setPokemon1] = useState(null);
-  const [pokemon2, setPokemon2] = useState(null);
+    const [pokemon1, setPokemon1] = useState(null);
+    const [pokemon2, setPokemon2] = useState(null);
 
     //set initialState for 2 empty pokemon
-  const initialState = [
-    {
-      pokemonObject: null, //Pokemon Obj
-      pokemonMovesTypes: [null, null, null, null],//4 type strings
-      pokemonScore: ''
-    },
-    {
-      pokemonObject: null,
-      pokemonMovesTypes: [null, null, null, null],
-      pokemonScore: ''
-    },
-    {
-      details: null,
-      pokemonAtAdvantage: null
-    }
+    const initialState = [
+        {
+            pokemonObject: null, //Pokemon Obj
+            pokemonMovesTypes: [null, null, null, null], //4 type strings
+            pokemonScore: "",
+        },
+        {
+            pokemonObject: null,
+            pokemonMovesTypes: [null, null, null, null],
+            pokemonScore: "",
+        },
+        {
+            details: null,
+            pokemonAtAdvantage: null,
+        },
+    ];
 
-  ]
+    const [pokemonInBattle, setPokemonInBattle] = useState(initialState);
 
+    ///Functions to update the state of the battlepage
+    const updatePokemonMoveTypes = (pokemonInd, moveToAdd, moveInd) => {
+        let newPokemonInBattle = [...pokemonInBattle];
+        newPokemonInBattle[pokemonInd].pokemonMovesTypes[moveInd] = moveToAdd;
+        setPokemonInBattle(newPokemonInBattle);
+    };
 
-  const [pokemonInBattle, setPokemonInBattle] = useState(initialState)
+    const updateDetails = (allInfo) => {
+        let newPokemonInBattle = [...pokemonInBattle];
+        newPokemonInBattle[2].details = allInfo;
+        setPokemonInBattle(newPokemonInBattle);
+    };
 
-  ///Functions to update the state of the battlepage 
-  const updatePokemonMoveTypes = (pokemonInd, moveToAdd, moveInd) =>{
-    let newPokemonInBattle = [...pokemonInBattle]
-    newPokemonInBattle[pokemonInd].pokemonMovesTypes[moveInd] = moveToAdd
-    setPokemonInBattle(newPokemonInBattle)
-  }
+    const updateScore = (score, index) => {
+        let newPokemonInBattle = [...pokemonInBattle];
+        newPokemonInBattle[index].pokemonScore = score;
+        setPokemonInBattle(newPokemonInBattle);
+    };
 
-  const updateDetails = (allInfo) =>{
-    let newPokemonInBattle = [...pokemonInBattle]
-    newPokemonInBattle[2].details = allInfo
-    setPokemonInBattle(newPokemonInBattle)
-  }
+    const updateWinner = (pokemonObj) => {
+        let newPokemonInBattle = [...pokemonInBattle];
+        newPokemonInBattle[2].pokemonAtAdvantage = pokemonObj;
+        setPokemonInBattle(newPokemonInBattle);
+    };
 
-  const updateScore = (score, index) =>{
-    let newPokemonInBattle = [...pokemonInBattle]
-    newPokemonInBattle[index].pokemonScore = score
-    setPokemonInBattle(newPokemonInBattle)
-  }
+    /// Object that contains the current state and functions to modify it
+    //Used to pass information between components
+    const pokemonState = {
+        pokemonInBattle: pokemonInBattle,
+        updatePokemonMoveTypes: updatePokemonMoveTypes,
+        updateDetails: updateDetails,
+        updateScore: updateScore,
+        updateWinner: updateWinner,
+    };
 
-  const updateWinner = (pokemonObj) =>{
-    let newPokemonInBattle = [...pokemonInBattle]
-    newPokemonInBattle[2].pokemonAtAdvantage = pokemonObj
-    setPokemonInBattle(newPokemonInBattle)
-  }
-  
-  /// Object that contains the current state and functions to modify it
-  //Used to pass information between components
-  const pokemonState = {
-    pokemonInBattle: pokemonInBattle,
-    updatePokemonMoveTypes: updatePokemonMoveTypes,
-    updateDetails: updateDetails,
-    updateScore: updateScore,
-    updateWinner: updateWinner
-  }
+    //Calculating 2 random ID for the first 151 pokemon
+    const id1 = Math.ceil(Math.random() * 151);
+    const id2 = Math.ceil(Math.random() * 151);
 
-  
-  //Calculating 2 random ID for the first 151 pokemon
-  const id1 = Math.ceil(Math.random() * 151);
-  const id2 = Math.ceil(Math.random() * 151);
-  
-  //Fetch the pokemon 1
-  const fetchPokemon1 = () => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id1}`)
-    .then((response) => response.json())
-    .then((response) => {
-      // console.log("res JSON", response);
-      setPokemon1(response);
-      let newPokemonInBattle = [...pokemonInBattle]
-      newPokemonInBattle[0].pokemonObject = response
-      setPokemonInBattle(newPokemonInBattle)
-      })
-      .catch(console.error);
-  };
+    //Fetch the pokemon 1
+    const fetchPokemon1 = () => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id1}`)
+            .then((response) => response.json())
+            .then((response) => {
+                // console.log("res JSON", response);
+                setPokemon1(response);
+                let newPokemonInBattle = [...pokemonInBattle];
+                newPokemonInBattle[0].pokemonObject = response;
+                setPokemonInBattle(newPokemonInBattle);
+            })
+            .catch(console.error);
+    };
 
-  //Fetch the pokemon 2
-  const fetchPokemon2 = () => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id2}`)
-      .then((response) => response.json())
-      .then((response) => {
-        // console.log("res JSON", response);
-        setPokemon2(response);
-        let newPokemonInBattle = [...pokemonInBattle]
-        newPokemonInBattle[1].pokemonObject = response
-        setPokemonInBattle(newPokemonInBattle)
-      })
-      .catch(console.error);
-  };
+    //Fetch the pokemon 2
+    const fetchPokemon2 = () => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id2}`)
+            .then((response) => response.json())
+            .then((response) => {
+                // console.log("res JSON", response);
+                setPokemon2(response);
+                let newPokemonInBattle = [...pokemonInBattle];
+                newPokemonInBattle[1].pokemonObject = response;
+                setPokemonInBattle(newPokemonInBattle);
+            })
+            .catch(console.error);
+    };
 
-  //Running the useEffect
-  useEffect(() => {
-    fetchPokemon1()
-    fetchPokemon2()
-}, [])
+    //Running the useEffect
+    useEffect(() => {
+        fetchPokemon1();
+        fetchPokemon2();
+    }, []);
 
-const card1 = Card(pokemon1, pokemonState, 0)
-const card2 = Card(pokemon2, pokemonState, 1)
+    const card1 = Card(pokemon1, pokemonState, 0);
+    const card2 = Card(pokemon2, pokemonState, 1);
 
-const resultsDiv = Results(pokemonInBattle, pokemonState) 
+    const resultsDiv = Results(pokemonInBattle, pokemonState);
 
-  return (
-    <>
-      <div className="battle-container">
-        {card1}
-        <h3>VS.</h3>
-        {card2}
-      </div>
-      <div className="details-container"> 
-        {pokemonInBattle[2].details}
-      </div>
-      <div className='results-container'>
-      {resultsDiv}
-      </div>
-
-    </>
-  )
-  }
+    return (
+        <>
+            <div className="battle-page">
+                <div className="battle-container">
+                    {card1}
+                    <h3 className="vs">VS.</h3>
+                    {card2}
+                </div>
+                <div className="details-container">
+                    {pokemonInBattle[2].details}
+                </div>
+                <div className="results-container">{resultsDiv}</div>
+            </div>
+        </>
+    );
+};
 
 export default BattlePage;
